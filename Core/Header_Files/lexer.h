65e2aacf7cd0e1bfcb70a4be2e5e5c4f3edb9ba7
Darkstalker <allanw.mk@gmail.com>
2025-04-17 12:40:54 -0400

    Update lexer.h

--- Core/Header_Files/lexer.h
+++ Core/Header_Files/lexer.h
@@ -1,61 +1,9 @@
-// create the lexer class
+#ifndef LEXER_H
+#define LEXER_H
 
-// Path: lexer.c
-#include <stdio.h>
-#include <ctype.h>
-#include <string.h>
+#include "parser.h"
 
-typedef enum {
-    TOKEN_IDENTIFIER,
-    TOKEN_NUMBER,
-    TOKEN_OPERATOR,
-    TOKEN_EOF
-} TokenType;
+void lexer_init(const char* source);
+Token get_next_token();
 
-typedef struct {
-    TokenType type;
-    char value[256];
-} Token;
-
-const char* input;
-size_t pos = 0;
-
-Token getNextToken() {
-    Token token;
-    while (isspace(input[pos])) pos++;
-
-    if (isalpha(input[pos])) {
-        size_t start = pos;
-        while (isalnum(input[pos])) pos++;
-        strncpy(token.value, &input[start], pos - start);
-        token.value[pos - start] = '\0';
-        token.type = TOKEN_IDENTIFIER;
-    } else if (isdigit(input[pos])) {
-        size_t start = pos;
-        while (isdigit(input[pos])) pos++;
-        strncpy(token.value, &input[start], pos - start);
-        token.value[pos - start] = '\0';
-        token.type = TOKEN_NUMBER;
-    } else if (input[pos] == '\0') {
-        token.type = TOKEN_EOF;
-    } else {
-        token.value[0] = input[pos];
-        token.value[1] = '\0';
-        token.type = TOKEN_OPERATOR;
-        pos++;
-    }
-
-    return token;
-}
-
-int main() {
-    input = "int x = 42 + y;";
-    Token token;
-    do {
-        token = getNextToken();
-        printf("Token: %s, Type: %d\n", token.value, token.type);
-    } while (token.type != TOKEN_EOF);
-
-    return 0;
-}
-
+#endif
