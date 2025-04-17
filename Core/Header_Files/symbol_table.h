e97e1862410130444e6c7304c1945b1f2a55bdf1
Darkstalker <allanw.mk@gmail.com>
2025-04-17 13:02:20 -0400

    Update symbol_table.h

--- Core/Header_Files/symbol_table.h
+++ Core/Header_Files/symbol_table.h
@@ -1,8 +1,8 @@
-// symbol_table.h - Symbol table header
+// Core/Header_Files/symbol_table.h - Symbol table for PyC compiler
 #ifndef SYMBOL_TABLE_H
 #define SYMBOL_TABLE_H
+#include <llvm-c/Core.h>
 
-// Symbol types
 typedef enum {
     SYMBOL_VARIABLE,
     SYMBOL_FUNCTION,
@@ -11,44 +11,28 @@ typedef enum {
     SYMBOL_IMPORT
 } SymbolType;
 
-// Symbol node structure
 typedef struct SymbolNode {
     char* name;
     SymbolType type;
-    void* data;          // Additional data associated with the symbol
-    int scope_id;        // Scope where this symbol is defined
+    void* data;          // Type info or metadata
+    LLVMValueRef llvm_value; // LLVM IR value for variables
+    int scope_id;
     struct SymbolNode* next;
 } SymbolNode;
 
-// Scope node structure
 typedef struct ScopeNode {
     int scope_id;
-    struct SymbolNode* symbols;
+    SymbolNode* symbols;
     struct ScopeNode* next;
 } ScopeNode;
 
-// Initialize the symbol table
 void init_symbol_table();
-
-// Enter a new scope
 void enter_scope();
-
-// Exit the current scope
 void exit_scope();
-
-// Add a symbol to the current scope
-void add_symbol(const char* name, SymbolType type, void* data);
-
-// Look up a symbol in the current scope only
+void add_symbol(const char* name, SymbolType type, void* data, LLVMValueRef llvm_value);
 SymbolNode* lookup_symbol_current_scope(const char* name);
-
-// Look up a symbol in all scopes (starting from the current one)
 SymbolNode* lookup_symbol(const char* name);
-
-// Print the current symbol table (for debugging)
 void print_symbol_table();
-
-// Clean up the symbol table
 void cleanup_symbol_table();
 
-#endif // SYMBOL_TABLE_H
+#endif
