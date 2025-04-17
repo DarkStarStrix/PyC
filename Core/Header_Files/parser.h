f266aa3c8835e8e6acce36fd64caf368d1f51c5c
Darkstalker <allanw.mk@gmail.com>
2025-04-17 12:42:02 -0400

    Update parser.h

--- Core/Header_Files/parser.h
+++ Core/Header_Files/parser.h
@@ -1,31 +1,32 @@
-// parser.h
-
 #ifndef PARSER_H
 #define PARSER_H
 
-#include "lexer.h"
-
 typedef enum {
-    AST_NODE_NUMBER __attribute__((unused)),
-    AST_NODE_IDENTIFIER __attribute__((unused)),
-    AST_NODE_BINARY_OP __attribute__((unused))
-} ASTNodeType;
+    TOKEN_NUMBER,
+    TOKEN_IDENTIFIER,
+    TOKEN_PLUS,
+    TOKEN_MINUS,
+    TOKEN_MULTIPLY,
+    TOKEN_DIVIDE,
+    TOKEN_ASSIGN,
+    TOKEN_COLON,
+    TOKEN_IF,
+    TOKEN_ELSE,
+    TOKEN_WHILE,
+    TOKEN_DEF,
+    TOKEN_INDENT,
+    TOKEN_DEDENT,
+    TOKEN_NEWLINE,
+    TOKEN_EOF
+} TokenType;
 
-typedef struct ASTNode {
-    __attribute__((unused)) ASTNodeType type;
-    __attribute__((unused)) union {
-        struct {
-            struct ASTNode* left;
-            struct ASTNode* right;
-            char op;
-        } binary_op;
-        char identifier[256];
-        int number;
-    } data;
-} ASTNode;
+typedef struct {
+    TokenType type;
+    char value[256];
+} Token;
 
-__attribute__((unused)) ASTNode* parseExpression();
+typedef struct ASTNode ASTNode;
 
-__attribute__((unused)) void freeAST(ASTNode* node);
+ASTNode* parse();
 
-#endif // PARSER_H
+#endif
