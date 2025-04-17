0b7524bdff5f59ef9e43a64e679b1b585ec5efc6
Darkstalker <allanw.mk@gmail.com>
2025-04-17 12:42:50 -0400

    Update parser.c

--- Core/C_Files/parser.c
+++ Core/C_Files/parser.c
@@ -1,83 +1,122 @@
 #include "parser.h"
+#include "lexer.h"
+#include "Core.h"
+#include "symbol_table.h"
 #include <stdlib.h>
-#include <stdio.h>
+#include <string.h>
 
-static Token currentToken;
+static Token current_token;
 
-static void getNextToken() {
-    currentToken = getNextToken();
+static void advance() {
+    current_token = get_next_token();
 }
 
-static ASTNode* createASTNode(ASTNodeType type) {
-    ASTNode* node = (ASTNode*)malloc(sizeof(ASTNode));
-    node->type = type;
-    return node;
-}
-
-static ASTNode* parsePrimary() {
-    ASTNode* node = NULL;
-    if (currentToken.type == TOKEN_NUMBER) {
-        node = createASTNode(AST_NODE_NUMBER);
-        node->data.number = atoi(currentToken.value);
-        getNextToken();
-    } else if (currentToken.type == TOKEN_IDENTIFIER) {
-        node = createASTNode(AST_NODE_IDENTIFIER);
-        strcpy(node->data.identifier, currentToken.value);
-        getNextToken();
-    } else {
-        printf("Syntax error: unexpected token %s\n", currentToken.value);
+static void expect(TokenType type) {
+    if (current_token.type != type) {
+        char msg[256];
+        sprintf(msg, "Expected token type %d, got %d", type, current_token.type);
+        report_error(lexer.line, msg);
         exit(1);
     }
+    advance();
+}
+
+static ASTNode* expression(); // Forward declaration
+
+static ASTNode* statement() {
+    ASTNode* node = malloc(sizeof(ASTNode));
+
+    if (current_token.type == TOKEN_IF) {
+        node->type = NODE_IF_STATEMENT;
+        advance();
+        node->if_stmt.condition = expression();
+        expect(TOKEN_COLON);
+        expect(TOKEN_NEWLINE);
+        expect(TOKEN_INDENT);
+        node->if_stmt.body = parse_block();
+        expect(TOKEN_DEDENT);
+        if (current_token.type == TOKEN_ELSE) {
+            advance();
+            expect(TOKEN_COLON);
+            expect(TOKEN_NEWLINE);
+            expect(TOKEN_INDENT);
+            node->if_stmt.else_body = parse_block();
+            expect(TOKEN_DEDENT);
+        } else {
+            node->if_stmt.else_body = NULL;
+        }
+    } else if (current_token.type == TOKEN_IDENTIFIER && 
+               get_next_token().type == TOKEN_ASSIGN) {
+        node->type = NODE_ASSIGNMENT;
+        strcpy(node->assign.name, current_token.value);
+        advance();
+        advance(); // Skip '='
+        node->assign.value = expression();
+        expect(TOKEN_NEWLINE);
+        add_variable(node->assign.name);
+    } else {
+        node->type = NODE_EXPRESSION;
+        node->expr = expression();
+        expect(TOKEN_NEWLINE);
+    }
     return node;
 }
 
-static ASTNode* parseBinaryOpRHS(int exprPrec, ASTNode* lhs) {
-    while (1) {
-        int tokPrec = (currentToken.type == TOKEN_OPERATOR) ? 10 : -1;
+static ASTNode* parse_block() {
+    ASTNode* block = malloc(sizeof(ASTNode));
+    block->type = NODE_BLOCK;
+    block->block.num_statements = 0;
+    block->block.statements = malloc(sizeof(ASTNode*) * 10); // Initial capacity
 
-        if (tokPrec < exprPrec) {
-            return lhs;
-        }
-
-        char op = currentToken.value[0];
-        getNextToken();
-
-        ASTNode* rhs = parsePrimary();
-
-        int nextPrec = (currentToken.type == TOKEN_OPERATOR) ? 10 : -1;
-        if (tokPrec < nextPrec) {
-            rhs = parseBinaryOpRHS(tokPrec + 1, rhs);
-        }
-
-        ASTNode* newLHS = createASTNode(AST_NODE_BINARY_OP);
-        newLHS->data.binary_op.left = lhs;
-        newLHS->data.binary_op.right = rhs;
-        newLHS->data.binary_op.op = op;
-        lhs = newLHS;
+    while (current_token.type != TOKEN_DEDENT && current_token.type != TOKEN_EOF) {
+        block->block.statements[block->block.num_statements++] = statement();
     }
-}
-
-ASTNode* parse_if_statement() {
-    expect_token(TOKEN_IF);
-    ASTNode* condition = parse_expression();
-    expect_token(TOKEN_COLON);
-    ASTNode* then_block = parse_block();
-    ASTNode* else_block = NULL;
-    if (current_token.type == TOKEN_ELSE) {
-        expect_token(TOKEN_ELSE);
-        expect_token(TOKEN_COLON);
-        else_block = parse_block();
-    }
-    return create_if_node(condition, then_block, else_block);
-}
-
-ASTNode* parse_block() {
-    expect_token(TOKEN_INDENT);
-    ASTNode* block = create_block_node();
-    while (current_token.type != TOKEN_DEDENT) {
-        ASTNode* stmt = parse_statement();
-        add_to_block(block, stmt);
-    }
-    expect_token(TOKEN_DEDENT);
     return block;
+}
+
+static ASTNode* expression() {
+    ASTNode* left = malloc(sizeof(ASTNode));
+    left->type = NODE_EXPRESSION;
+
+    if (current_token.type == TOKEN_NUMBER) {
+        left->expr.type = EXPR_NUMBER;
+        strcpy(left->expr.value, current_token.value);
+        advance();
+    } else if (current_token.type == TOKEN_IDENTIFIER) {
+        left->expr.type = EXPR_VARIABLE;
+        strcpy(left->expr.value, current_token.value);
+        if (!check_variable(left->expr.value)) {
+            report_error(lexer.line, "Undefined variable");
+            exit(1);
+        }
+        advance();
+    }
+
+    if (current_token.type == TOKEN_PLUS || current_token.type == TOKEN_MINUS) {
+        ASTNode* binop = malloc(sizeof(ASTNode));
+        binop->type = NODE_BINARY_OP;
+        binop->binop.left = left;
+        binop->binop.op = current_token.type;
+        advance();
+        binop->binop.right = expression();
+        return binop;
+    }
+    return left;
+}
+
+ASTNode* parse() {
+    advance();
+    ASTNode* root = malloc(sizeof(ASTNode));
+    root->type = NODE_BLOCK;
+    root->block.num_statements = 0;
+    root->block.statements = malloc(sizeof(ASTNode*) * 10);
+
+    while (current_token.type != TOKEN_EOF) {
+        if (current_token.type == TOKEN_NEWLINE) {
+            advance();
+            continue;
+        }
+        root->block.statements[root->block.num_statements++] = statement();
+    }
+    return root;
 }
